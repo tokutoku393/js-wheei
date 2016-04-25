fc21e35390a7295a0d8ab063e8db14244f9eef08
yuka <la_boo_u@live.jp>
2016-04-25 15:03:26 +0900

    test

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+var express = require('express');
+var app = express();
+
+
+app.set('port', process.env.PORT || 5000);
+
+
+app.use(express.logger('dev'));
+app.use(express.compress());
+app.use(express.static(__dirname));
+
+app.listen(app.get('port'), function() {
+  console.log('Server listening on port %s', app.get('port'));
+});
+
+
+/*
 var fs = require('fs');
 var http = require('http');
 var server = http.createServer();
@@ -7,8 +24,10 @@ server.on('request', function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   stream.pipe(res);
 });
+*/
+
 //var io = require('socket.io').listen(server);
-server.listen(process.env.PORT || 5000);
+//server.listen(process.env.PORT || 5000);
 
 //io.sockets.on('connection', function(socket) {
 //  socket.on("mode", function(sent){
